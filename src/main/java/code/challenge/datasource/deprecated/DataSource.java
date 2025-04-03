822747a7ccbdf059f2de614abcfdc78f781b9a44
gnush <1129861+gnush@users.noreply.github.com>
2025-04-03 12:31:23 +0200

    removed deprecated

--- src/main/java/code/challenge/datasource/deprecated/DataSource.java
+++
@@ -1,9 +0,0 @@
-package code.challenge.datasource.deprecated;
-
-import code.challenge.product.deprecated.NullableExpirationProduct;
-
-import java.util.List;
-
-public interface DataSource {
-    List<NullableExpirationProduct> getProducts();
-}
