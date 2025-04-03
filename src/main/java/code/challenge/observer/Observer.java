00e049b28605f539553a8f863fdd63f7da548f72
gnush <1129861+gnush@users.noreply.github.com>
2025-04-03 12:18:51 +0200

    keep deprecated structure

--- src/main/java/code/challenge/observer/Observer.java
+++
@@ -1,7 +0,0 @@
-package code.challenge.observer;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Observer<Subject, Delta> {
-    void update(@NotNull Subject subject, @NotNull Delta delta);
-}
